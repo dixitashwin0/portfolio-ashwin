d7d5800caa7c6b958866c18b316dfe72ad0801aa
Ashwin Dixit <dixitashwin0@gmail.com>
2023-08-28 23:14:19 +0530

    iphone mini settings

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,12 @@ import {Link} from 'react-scroll'
 const Home = () => {
   return (
     <div name="Home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-600'>
-     <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+     <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 pt-20 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
-            <h2 className='text-3xl sm:text-5xl font-bold text-white'>
+            <h2 className='text-3xl sm:text-7xl font-bold text-white'>
                 I'm a Mechanical Engineer coding all of it around.
             </h2>
-            <p className='text-gray-500 py-4'>
+            <p className='text-gray-500  max-w-md py-4'>
            Me being 20 year old engineer with hands-on experience in MERN STACK Development   and technologies like TailwindCSS.I also have working level knowledge about SQL and MongoDb.
            I have working level knowledge in Java.
            I possess knowledge about  designing and designing softwares such as SolidWorks.
